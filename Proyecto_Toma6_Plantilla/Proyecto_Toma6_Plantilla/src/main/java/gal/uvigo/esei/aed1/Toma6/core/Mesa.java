c455673f35c163abac498a51df41116ed324aa81
adrixprietox <113402195+adrixprietox@users.noreply.github.com>
2024-04-18 11:04:01 +0200

    crear clase mesa

---
+++ Proyecto_Toma6_Plantilla/Proyecto_Toma6_Plantilla/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Mesa.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gal.uvigo.esei.aed1.Toma6.core;
+
+/**
+ *
+ * @author adrian
+ */
+public class Mesa {
+    
+    private Carta[][] cuatroSobrantes; // Array de cartas bidimensional para representar las filas de cartas sobrantes
+    private final int numFilas= 4; // Número de filas
+    private final int numMaxCol= 5; // Máximo de cartas por columna
+
+    public Mesa() {
+        cuatroSobrantes = new Carta[numFilas][numMaxCol]; // Inicializar el array bidimensional
+        // Llenar las columnas con cartas iniciales
+        for (int i = 0; i < numFilas; i++) {
+            for (int j = 0; j < numMaxCol; j++) {
+                // Creamos una nueva carta con valores específicos
+                cuatroSobrantes[i][j] = new Carta(numFilas,numMaxCol); // Supongamos que la carta inicial tiene valores 0 y 0
+            }
+        }
+    }
+
+    public void contenidoMesa (){
+        for (int i = 0; i < numFilas; i++){
+            for (int j = 0; j < numMaxCol; i++){
+                System.out.println(cuatroSobrantes);
+            }
+        }
+        
+        
+    }
+
+    
+    
+}
